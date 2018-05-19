0d649bb7346a7cfe6abf6597f81b878ec2463822
Paul Shannon <pshannon@sitepen.com>
2018-05-18 17:08:29 -0700

    WIP build and test working

---
+++ support/process.ts
@@ -0,0 +1,79 @@
+import { spawn, ChildProcess } from 'child_process';
+
+export class ProcessError extends Error {
+	constructor(
+		public readonly code: number,
+		public readonly stdErr = '',
+		public readonly stdOut = ''
+	) {
+		super(`Error exit code ${ code }`);
+	}
+}
+
+export function spawnCommand(command: string, args: string[], options: any = {}): ChildProcess {
+	return spawn(command, args, {
+		shell: true,
+		... options
+	});
+}
+
+export async function awaitProcess(childProcess: ChildProcess) {
+	return new Promise<number>((resolve, reject) => {
+		childProcess.once('close', (code: number) => {
+			if (code <= 0) {
+				resolve(code);
+			}
+			else {
+				reject(new ProcessError(code));
+			}
+		});
+	});
+}
+
+export function collectOutput(childProcess: ChildProcess) {
+	let stderr = '';
+	let stdout = '';
+
+	if (childProcess.stdout) {
+		childProcess.stdout.setEncoding('utf8');
+		childProcess.stdout.on('data', (chunk) => {
+			stdout += chunk;
+		});
+	}
+
+	if (childProcess.stderr) {
+		childProcess.stderr.setEncoding('utf8');
+		childProcess.stderr.on('data', (chunk) => {
+			stderr += chunk;
+		});
+
+	}
+
+	return {
+		getOutput() {
+			return stdout;
+		},
+		getError() {
+			return stderr;
+		}
+	}
+}
+
+export async function runAsPromise(command: string, args: string[], options: any = {}) {
+	const process = spawnCommand(command, args, options);
+	const output = collectOutput(process);
+	let code: number;
+
+	try {
+		code = await awaitProcess(process);
+	}
+	catch (e) {
+		const code = 'code' in e ? e.code : 1;
+		throw new ProcessError(code, output.getError(), output.getOutput());
+	}
+
+	return {
+		code,
+		... output
+	};
+}
--- support/publish.ts
+++ support/publish.ts
@@ -1,9 +1,9 @@
 import { join, resolve } from 'path';
 import * as yargs from 'yargs';
-
-const runAsPromise = require('@dojo/scripts/utils/process').runAsPromise;
+import { awaitProcess, runAsPromise, spawnCommand } from './process';
 
 const binDir = join(__dirname, '..', 'node_modules', '.bin');
+const packageDir = join(__dirname, '..', 'packages');
 const lernaBin = resolve(binDir, 'lerna');
 const argv: any = yargs.option({
 	'dry-run': {
@@ -21,53 +21,93 @@ const argv: any = yargs.option({
 }).argv;
 const baseBranch = 'master';
 
-interface runAsPromise {
-	(command: string, args: string[], options: any): Promise<any>;
-}
-
 interface Updated {
 	name: string;
 	version: string;
 	private: boolean;
 }
 
-async function publish() {
-	const updatedPackages: Updated[] = JSON.parse(await runAsPromise(lernaBin, [ 'updated', '--json']));
-	await lernaPublish();
+const runToTerminalOptions = Object.freeze({
+	stdio: [ null, process.stdout, process.stdin ]
+});
 
-	await build();
-	await publishToNpm();
-	await updateWorkingVersion();
-	await pushGit();
-	console.log(updatedPackages);
+async function publish() {
+	const updatedPackagesResult = (await runAsPromise(lernaBin, [ 'updated', '--json'])).getOutput();
+	const updatedPackages: Updated[] = JSON.parse(updatedPackagesResult);
+
+	try {
+		await lernaPublish();
+		await prebuildChecks(updatedPackages);
+		await build();
+		await publishToNpm();
+		await pushGit();
+		console.log(`published ${ updatedPackages.map((pack) => pack.name ).join(', ')}`)
+	}
+	catch(e) {
+		console.log('error', e);
+		throw e;
+	}
 }
 
-function lernaPublish() {
+/**
+ * Use lerna to publish a new release
+ *
+ * 1. Prompts the user for a version
+ * 2. Updates package.json version
+ * 3. Creates a git commit
+ * 4. tags the commit
+ */
+async function lernaPublish() {
 	const args = [ 'publish', '--skip-npm', '--message', '"Release %s"'];
 	if (argv.releaseVersion) {
 		args.push(`--repo-version=${ argv.releaseVersion }`);
 	}
-	return runAsPromise(lernaBin, args, {
+	const command = spawnCommand(lernaBin, args, {
 		stdio: [ process.stdin, process.stdout, process.stderr ]
 	});
+
+	return await awaitProcess(command);
 }
 
-async function build() {
-	await runAsPromise(lernaBin, [ 'run', 'clean', '--since', baseBranch]);
-	if (!argv.skipTest) {
-		await runAsPromise(lernaBin, [ 'run', 'test', '--since', baseBranch]);
+/**
+ * Ensure all packages being released have
+ *
+ * 1. Had their package.json versions updated to match
+ * 2. There is a matching git tag (--dry-run on the command line will skip this check)
+ */
+async function prebuildChecks(updatedPackages: Updated[]) {
+	const versions = updatedPackages.map((pack) => {
+		const name = pack.name.substr(pack.name.indexOf('/'));
+		const packageJsonPath = join(packageDir, name, 'package.json');
+		const packagejson = require(packageJsonPath);
+		return packagejson.version;
+	});
+
+	const releaseVersion = versions[0];
+	for (let version of versions) {
+		if (version !== releaseVersion) {
+			throw new Error(`Version mismatch. Expected ${ releaseVersion }. Saw ${ version }.`);
+		}
 	}
-	await runAsPromise(lernaBin, [ 'run', 'dist', '--since', baseBranch]);
+
+	if (!argv.dryRun) {
+		await runAsPromise('git', [ 'describe', '--tags', `v${ releaseVersion }` ], runToTerminalOptions);
+	}
+}
+
+/**
+ * Builds all of the packages to be published
+ */
+async function build() {
+	await runAsPromise(lernaBin, [ 'run', 'clean', '--since', baseBranch], runToTerminalOptions);
+	if (!argv.skipTest) {
+		await runAsPromise(lernaBin, [ 'run', 'test', '--since', baseBranch], runToTerminalOptions);
+	}
+	await runAsPromise(lernaBin, [ 'run', 'dist', '--since', baseBranch], runToTerminalOptions);
 }
 
 async function publishToNpm() {
 	// TODO publish package
-}
-
-async function updateWorkingVersion() {
-	if (baseBranch === 'master') {
-		// TODO update package.json to x.x.(x+1)-pre,
-	}
 }
 
 async function pushGit() {
